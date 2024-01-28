f29f5209359211c42578ad2b31cf96b1daca4c73
linuxlonelyeagle <lonelyeagle.02@bytedance.com>
2024-01-28 17:06:26 +0800

    update enable_if_t.cpp

--- enable_if_t.cpp
+++ enable_if_t.cpp
@@ -29,11 +29,44 @@ void print1(T size0) {
   return;
 };
 
+template<typename T, typename Tag = int>
+class A {
+  T data0;
+  Tag data1;
+  public:
+    A() = default;
+    A(T data0, Tag data1) : data0 {data0}, data1 {data1} { }
+    void print() {
+      cout << "normal version." << endl;
+      cout << "data0:" << data0 << endl;
+      cout << "data1:" << data1 << endl; 
+    }
+};
+
+template<typename T>
+class A<T, std::enable_if_t<std::is_same_v<T, std::string>, std::string>> {
+  T data0; 
+  using Tag = std::enable_if_t<std::is_same_v<T, std::string>, std::string>;
+  Tag data1;
+  public:
+     A() = default;
+     A(T data0, Tag data1) : data0 {data0}, data1 {data1} { }
+     void print() {
+      cout << "std::string version." << endl;
+      cout << "data0:" << data0 << endl;
+      cout << "data1:" << data1 << endl;
+     }
+};
+
 
 int main() {
   print0(2);
   print0(2.0);
   print1(static_cast<int>(10));
   print1(static_cast<size_t>(10));
+  A a0(10, 10);
+  a0.print();
+  A a1(std::string("hello"), std::string("world"));
+  a1.print();
   return 0;
 }
